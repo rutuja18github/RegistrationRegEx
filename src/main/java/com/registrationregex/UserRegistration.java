e4165c6696d6ba7eaa34d811560948a93bfdc4b1
RutujaPatil18 <prutuja189@gmail.com>
2022-10-03 14:47:15 +0530

    Modify:PassWord Checking

--- src/main/java/com/registrationregex/UserRegistration.java
+++ src/main/java/com/registrationregex/UserRegistration.java
@@ -14,78 +14,109 @@ public class UserRegistration {
 	public void fillForm() {
 		logger.info("Enter first name Start with Capital letter and has atleast 3 charecter");
 		String firstName = scanner.nextLine();
-		validateFirstName(firstName);
+		boolean firstNameResult = validateFirstName(firstName);
+		while (firstNameResult == false) {
+			logger.info("Enter valid First name");
+			String newFirstName = scanner.nextLine();
+			boolean value = validateFirstName(newFirstName);
+			if (value) {
+				break;
+			}
+		}
 		logger.info("Enter last name Start with Capital letter and has atleast 3 charecter");
 		String LastName = scanner.nextLine();
-		validateLastName(LastName);
+		boolean lastNameResult = validateLastName(LastName);
+		while (lastNameResult == false) {
+			logger.info("Enter valid Last name");
+			String newLasttName = scanner.nextLine();
+			boolean result = validateLastName(newLasttName);
+			;
+			if (result) {
+				break;
+			}
+		}
 		logger.info("Enter Email ID e.g. abc.xyz@bl.co.in");
 		String email = scanner.nextLine();
-		validateEmail(email);
+		boolean emailResult = validateEmail(email);
+		while (emailResult == false) {
+			logger.info("Enter valid email ");
+			String newemail = scanner.nextLine();
+			boolean result = validateEmail(newemail);
+			if (result) {
+				break;
+			}
+		}
 		logger.info("Enter mobile number country code followed by space and 10 digit number");
 		String mobile = scanner.nextLine();
-		validateMobile(mobile);
+		boolean mobileResult=validateMobile(mobile);
+		while (mobileResult == false) {
+			logger.info("Enter valid mobile number ");
+			String newMobile = scanner.nextLine();
+			boolean value =validateMobile(newMobile);
+			if (value) {
+				break;
+			}
+		}
 		logger.info("Enter a PassWord");
 		String password = scanner.nextLine();
-		validatePassword(password);
+		boolean passwordResult=validatePassword(password);
+		while (passwordResult == false) {
+			logger.info("Enter valid password");
+			String newPassWord = scanner.nextLine();
+			boolean value = validatePassword(newPassWord);
+			if (value) {
+				break;
+			}
+		}
+
 	}
 
 	public boolean validateFirstName(String firstName) {
 		Pattern patternObject = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
 		Matcher matcher = patternObject.matcher(firstName);
-		if (matcher.matches() == false) {
-			System.out.println("Invalid");
-			logger.info("Enter valid First name");
-			String newFirstName = scanner.nextLine();
-			validateFirstName(newFirstName);
+		if (matcher.matches()) {
+			return true;
+		} else {
+			return false;
 		}
-		return matcher.matches();
 	}
 
 	public boolean validateLastName(String lastName) {
 		Pattern patternObject = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
 		Matcher matcher = patternObject.matcher(lastName);
-		if (matcher.matches() == false) {
-			System.out.println("Invalid");
-			logger.info("Enter valid Last name");
-			String newLasttName = scanner.nextLine();
-			validateFirstName(newLasttName);
+		if (matcher.matches()) {
+			return true;
+		} else {
+			return false;
 		}
-		return matcher.matches();
 	}
 
 	public boolean validateEmail(String email) {
-		Pattern patternObject = Pattern.compile("^[abc]{3}[a-zA-Z0-9.]*@[bl]{2}[.][co]{2}[.][a-z.]*$");
+		Pattern patternObject = Pattern.compile("^[A-Za-z0-9+.-]*@[a-z0-9.]*$");
 		Matcher matcher = patternObject.matcher(email);
-		if (matcher.matches() == false) {
-			System.out.println("Invalid");
-			logger.info("Enter valid email ");
-			String newemail = scanner.nextLine();
-			validateFirstName(newemail);
+		if (matcher.matches()) {
+			return true;
+		} else {
+			return false;
 		}
-		return matcher.matches();
 	}
-
 	public boolean validateMobile(String mobile) {
 		Pattern patternObject = Pattern.compile("^[91]{2}[ ]{1}[0-9]{10}$");
 		Matcher matcher = patternObject.matcher(mobile);
-		if (matcher.matches() == false) {
-			System.out.println("Invalid");
-			logger.info("Enter valid email ");
-			String newMobile = scanner.nextLine();
-			validateFirstName(newMobile);
+		if (matcher.matches()) {
+			return true;
+		} else {
+			return false;
 		}
-		return matcher.matches();
 	}
-
 	public boolean validatePassword(String password) {
 		Pattern patternObject = Pattern.compile("^[a-zA-Z0-9]{8,}$");
 		Matcher matcher = patternObject.matcher(password);
-		if (matcher.matches() == false) {
-			System.out.println("Invalid");
-			logger.info("Enter valid email ");
-			String newMobile = scanner.nextLine();
-			validateFirstName(newMobile);
+		if (matcher.matches()) {
+			return true;
+		} else {
+			return false;
 		}
-		return matcher.matches();
 	}
+
 }
